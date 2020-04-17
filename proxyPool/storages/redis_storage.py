021fc10528e600f0afd763cbbe2f9b3008786f4a
zlj-zz <zlj19971222@outlook.com>
2020-04-17 09:12:46 +0800

    change

--- proxyPool/storages/redis_storage.py
+++
@@ -1,9 +0,0 @@
-import redis
-import sys
-sys.path.append('../../proxyPool')
-# from proxypool.exceptions import PoolEmptyException
-# from proxypool.schemas.proxy import Proxy
-# from proxypool.setting import REDIS_HOST, REDIS_PORT, REDIS_PASSWORD, REDIS_KEY, PROXY_SCORE_MAX, PROXY_SCORE_MIN, PROXY_SCORE_INIT
-
-from random import choice
-from typing import List
